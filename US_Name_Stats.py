b1d11abeb2c4538d9d94d7f136ba810003ef4136
Dylan Martin <dylan.martin7675@gmail.com>
2019-02-20 18:46:31 -0800

    first commit

---
+++ US_Name_Stats.py
@@ -0,0 +1,1 @@
+"""This is the main script for my US_Name_Stats data anylysis project"""
---
+++ data_format.py
@@ -0,0 +1,2 @@
+"""This is a helper script that adds a header formatting to all files,
+and converts all files from .txt to .csv"""
