0627371611ea6c95bd44d85667c2e585f94391c1
Dylan Martin <dylan.martin7675@gmail.com>
2019-02-22 01:05:15 -0800

    finalized the data_format script. This script adds a header above the data, adds the year of birth within the file per row, and converts the file extension from txt to csv

--- data_format.py
+++ data_format.py
@@ -1,17 +1,24 @@
-"""This is a helper script that adds a header formatting to all files,
-and converts all files from .txt to .csv"""
-
 def txt_to_csv():
+
+    """This is a helper script that adds a header formatting to all files,
+and converts them from .txt to .csv"""
+
     import os
 
-    header = "Name,Sex,Quantity"
+    output = ""
+    header = "Name,Sex,Quantity,Year"
     os.chdir("./Year_of_Birth_Data")
+
     for file in os.listdir("."):
         if file.endswith(".txt"):
             base = os.path.splitext(file)[0]
-            with open(file, 'r') as f_in: data = f_in.read()
+            year = base[3:]
+
+            with open(file, 'r') as f_in: #line[:-1] because of a carriage return as last value of line
+                data = [ line[:-1] + (",%s" %year)) for line in f_in]
             with open(file, 'w') as f_out:
-                f_out.write("%s\n%s" %(header, data))
+                f_out.write("%s\n" %header)
+                for entry in data: f_out.write("%s\n" % entry)
             os.rename(file, base + ".csv")
             print(file + " Formatted")
 
