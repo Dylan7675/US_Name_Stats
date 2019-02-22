ab8034985eb4790c096d76bbb8f6a3eb947fb194
Dylan Martin <dylan.martin7675@gmail.com>
2019-02-21 22:08:48 -0800

    added file extension conversion function

--- data_format.py
+++ data_format.py
@@ -1,2 +1,28 @@
 """This is a helper script that adds a header formatting to all files,
 and converts all files from .txt to .csv"""
+
+def txt_to_csv():
+    import os
+
+    header = "Name,Sex,Quantity"
+    os.chdir("./Year_of_Birth_Data")
+    for file in os.listdir("."):
+        if file.endswith(".txt"):
+            base = os.path.splitext(file)[0]
+            with open(file, 'r') as f_in: data = f_in.read()
+            with open(file, 'w') as f_out:
+                f_out.write("%s\n%s" %(header, data))
+            os.rename(file, base + ".csv")
+            print(file + " Formatted")
+
+
+def main():
+
+    print("Converting all files from .txt to .csv and applying a header")
+    txt_to_csv()
+    print("All files have been converted and formatted")
+
+
+if __name__ == "__main__":
+    main()
+
