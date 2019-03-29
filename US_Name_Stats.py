b44e321ad4c923ea855fd85c3108cd8ac2aca1fc
Dylan7675 <dylan.martin7675@gmail.com>
2019-03-28 22:41:32 -0700

    updated the function to create a pandas DataFrame for every unique name that holds all the according data

--- US_Name_Stats.py
+++ US_Name_Stats.py
@@ -2,31 +2,37 @@
 import os
 import pandas as pd
 import numpy as np
-
+import time
 
 def main():
 
     name_dic = csv_to_name_dic()
+    print(name_dic['Dylan'])
 
 
 def csv_to_name_dic():
 
     name_dic = {}
+    header = ['Sex', 'Qty', 'Year']
 
     for file in os.listdir("./Year_of_Birth_Data"):
-        print("loading data from {}".format(file))
+        #print("loading data from {}".format(file))
         with open("./Year_of_Birth_Data/{}".format(file), 'r') as f_in:
             for line in f_in:
                 data = line.split(',')
                 key = data[0]
                 data[3] = data[3][:-1] #carraige return at the end of each line
                 values = data[1:]
+                tempdf = pd.DataFrame([values], columns = header)
 
                 if key not in name_dic.keys():
-                    name_dic[key] = values
-
+                    name_dic[key] = tempdf
+                    print(name_dic[key])
+                    #time.sleep(2)
                 else:
-                    name_dic[key].append(values)
+                    name_dic[key] = pd.concat([name_dic[key], tempdf], ignore_index = True)
+                    print(name_dic[key])
+                    #time.sleep(2)
 
     return(name_dic)
 
