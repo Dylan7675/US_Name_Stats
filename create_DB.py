de76f2739d59ab9f8cb0aa3bb1f4b5e5c6563431
Dylan Martin <dylan.martin7675@gmail.com>
2019-06-21 22:41:18 -0700

    create_db.py completed.

--- create_DB.py
+++ create_DB.py
@@ -5,7 +5,7 @@ import pandas as pd
 import time
 import sqlalchemy
 from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey
-from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import exc
 import sys
 
 def main():
@@ -22,33 +22,51 @@ def create_db(name_dic):
     total_keys  = len(name_dic.keys())
     total_years = len(year_list)
 
-
     engine = create_engine("mysql+pymysql://USER:PASSWORD@localhost/DB", echo=False)
     connection = engine.connect()
     metadata = MetaData(engine)
+    metadata.reflect(engine)
+    db_tables = set(metadata.tables.keys())
 
-    name_table =  Table('Names', metadata,
-                  Column('id', Integer, primary_key=True),
-                  Column('name', String(30)))
+    if "Names" in db_tables:
+        print("Names Table already exists")
+        name_table = Table('Names', metadata)
 
-    year_table =  Table('Years', metadata,
-                  Column('id', Integer, primary_key=True),
-                  Column('year', Integer))
+    else:
+        name_table =  Table('Names', metadata,
+            Column('id', Integer, primary_key=True),
+            Column('name', String(30)))
 
-    metadata.create_all(engine)
+    if "Years" in db_tables:
+        print("Years Table already exists")
+        year_table = Table('Years', metadata)
+
+    else:
+        year_table =  Table('Years', metadata,
+            Column('id', Integer, primary_key=True),
+            Column('year', Integer))
+
+    try:
+        metadata.create_all(engine)
+
+    except exc.SQLAlchemyError as e:
+        print("Tables already existed")
 
     print("Loading DataFrames to Database Tables")
 
     for progress,k in enumerate(name_dic.keys(), 1):
 
+        if k in db_tables:
+            update_progress(progress/total_keys)
+            continue
+
         try:
             name_dic[k].to_sql(k, con=engine, if_exists='replace')
-        except SQLAlchemyError as e:
+        except exc.SQLAlchemyError as e:
             print("****Couldn't insert {0} table. Investigate!****".format(k))
 
         ins = name_table.insert().values(name=k)
         connection.execute(ins)
-        time.sleep(1)
 
         update_progress(progress/total_keys)
 
@@ -65,6 +83,7 @@ def create_db(name_dic):
     print("Done Loading Database")
 
 
+
 def csv_to_DataFrame():
 
     name_dic = {}
@@ -78,15 +97,18 @@ def csv_to_DataFrame():
 
         with open("./Year_of_Birth_Data/{}".format(file), 'r') as f_in:
             for line in f_in:
-                data = line.split(',')
-                key = data[0]
-                data[3] = data[3][:-1] #carraige return at the end of each line
-                values = data[1:]
-                tempdf = pd.DataFrame([values], columns = header)
+                try:
+                    data = line.split(',')
+                    key = data[0]
+                    data[2] = int(data[2])
+                    data[3] = int(data[3][:-1]) #carraige return at the end of each line
+                    values = data[1:]
+                    tempdf = pd.DataFrame([values], columns = header)
+                except ValueError as e:
+                    continue #offsetting for the header of every file, throws value error converting text to int
 
                 if key not in name_dic.keys():
                     name_dic[key] = tempdf
-                    #print(name_dic[key])
                 else:
                     name_dic[key] = pd.concat([name_dic[key], tempdf], ignore_index = True)
 
