772f753189e94133eb03188cf642d481d03fee49
Dylan Martin <dylan.martin7675@gmail.com>
2019-06-26 22:36:06 -0700

    Added the capability to query the DB into a DF. Also added a script to plot the results of that DF.

--- query_to_df.py
+++ query_to_df.py
@@ -5,8 +5,7 @@ from sqlalchemy.sql import select
 from connector import connect
 
 def query():
-"""query() takes in the selections for Name and Sex as imput and queries the Database.
-   Returns the result of the query stored as a dataframe """
+    """query() takes in the selections for Name and Sex as input and queries the Database. Returns the result of the query stored as a dataframe """
     connection, metadata, engine = connect()
 
     #Take in a name an sex as input, will change to take input from Django
@@ -17,7 +16,6 @@ def query():
     if not engine.dialect.has_table(engine, name):
         print("{0} does not exist in the dataset".format(name))
     else:
-        print("{0} exists in the dataset".format(name))
         query = "Select * from {0} where {0}.Sex in ('{1}') order by {0}.Year".format(name, sex)
         df = pd.read_sql(query, connection)
 
