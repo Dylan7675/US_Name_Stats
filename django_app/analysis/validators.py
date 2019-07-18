d975d020f834f40ba40f017cb3ad1cd45ad9c7e6
Dylan Martin <dylan.martin7675@gmail.com>
2019-07-18 00:48:00 -0700

    fixing repo

--- django_app/analysis/validators.py
+++
@@ -1,7 +0,0 @@
-from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _
-import re
-
-def name_validation(val):
-    if not re.match('^[a-zA-Z]+$', val):
-        raise ValidationError("Enter a Valid Name(Letters Only)")
--- django_app/name_stats/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup
-
-setup(name='US_Name_Stats',
-      version='1.0',
-      py_modules=['bokeh_plot', 'query_to_df', 'connector',
-                  'create_DB', 'data_format'])
